325b3fab8ce222b7425eaaaac53619e3cdc88fd5
Geovane <geovanestuski2@gmail.com>
2024-08-11 13:36:55 -0300

    Creation of routes, adding create and list products and categories, adding uploads

---
+++ src/app/useCases/categories/createCategory.ts
@@ -0,0 +1,15 @@
+import {Request, Response} from 'express';
+
+import { Category } from '../../models/Category';
+
+export async function createCategory(req: Request, res: Response) {
+  try {
+    const { icon, name } = req.body;
+
+    const response = await Category.create({icon, name});
+
+    res.json(response);
+  } catch {
+    res.status(500).json({error: 'Error to create category'});
+  }
+}
---
+++ src/router.ts
@@ -0,0 +1,50 @@
+import { Router } from 'express';
+import path from 'node:path';
+import multer from 'multer';
+
+import { listCategories } from './app/useCases/categories/listCategory';
+import { createCategory } from './app/useCases/categories/createCategory';
+
+import {listProducts} from './app/useCases/products/listProducts';
+import {createProduct} from './app/useCases/products/createProduct';
+
+export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req,file,callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
+  })
+});
+
+router.get('/categories', listCategories);
+
+router.post('/categories', createCategory);
+
+router.get('/products', listProducts);
+
+router.post('/products', upload.single('image'), createProduct);
+
+router.get('/categories/:categoryId/products', (req, res) => {
+  res.send('ok');
+});
+
+router.get('/orders', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/orders', (req, res) => {
+  res.send('ok');
+});
+
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
+
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
