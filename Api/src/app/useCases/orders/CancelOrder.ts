2204c35487846cc4a98afa764eab80a74af0b8cf
Geovane <geovanestuski2@gmail.com>
2024-10-14 19:11:21 -0300

    Fix files name

--- Api/src/app/useCases/orders/CancelOrder.ts
+++ Api/src/app/useCases/orders/CancelOrder.ts
@@ -1,7 +1,7 @@
 import { Types } from 'mongoose';
 import OrdersRepository from '../../repositories/OrdersRepository';
 
-export async function CancelOrder(id: string) {
+export async function CancelOrder(id: Types.ObjectId) {
   const ida = new Types.ObjectId(id);
   const order = await OrdersRepository.delete(ida);
 
